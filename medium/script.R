2df50df0ada91fe1df52641d88548a81ab0493a8
khaledlooda <36304731+khaledlooda@users.noreply.github.com>
2020-03-15 22:58:12 +0200

    Add files via upload

---
+++ medium/script.R
@@ -0,0 +1,34 @@
+dataframes_found_in_system=c("DATA.1","DATA.2","DATA.3","DATA.4")
+#loading the required packages
+
+library(seqinr)
+library(ggplot2)
+
+#reading fsa file using read.abif() and taking data portion only
+
+data<-read.abif("A01-Ladder-PP16-001.20sec.fsa")$Data
+
+#Data plotting
+
+
+dataframes=NULL
+i=length(dataframes_found_in_system)
+
+while(i!=0)
+{
+time<-c(1:length(data[[dataframes_found_in_system[i]]]))
+data_number<-rep(dataframes_found_in_system[i],length(data[[dataframes_found_in_system[i]]]))
+value<-data[[dataframes_found_in_system[i]]]
+temp<-data.frame(time,data_number,value)
+dataframes=rbind(dataframes,temp)
+i=i-1
+}
+
+
+
+#plotting multi-panel ggplot with facet_grid
+ggplot(data = dataframes,group=data_number,aes(color=data_number)) +
+  geom_line(data = dataframes,aes(x=time,y=value))+facet_grid(data_number~.,scales = "free")+
+  scale_x_continuous(name = "Time/1000")+scale_y_continuous(name = "RFU/1000")
+
+
