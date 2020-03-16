294093df4101bbe9ca49adc9890e48a97f0e919e
khaledlooda <36304731+khaledlooda@users.noreply.github.com>
2020-03-16 13:42:22 +0200

    Update script.R

--- medium/script.R
+++ medium/script.R
@@ -1,3 +1,4 @@
+#Add your dataframes here
 dataframes_found_in_system=c("DATA.1","DATA.2","DATA.3","DATA.4")
 #loading the required packages
 
@@ -14,6 +15,7 @@ data<-read.abif("A01-Ladder-PP16-001.20sec.fsa")$Data
 dataframes=NULL
 i=length(dataframes_found_in_system)
 
+#while there is dataframe take it and bind it with previous dataframes
 while(i!=0)
 {
 time<-c(1:length(data[[dataframes_found_in_system[i]]]))
@@ -30,5 +32,3 @@ i=i-1
 ggplot(data = dataframes,group=data_number,aes(color=data_number)) +
   geom_line(data = dataframes,aes(x=time,y=value))+facet_grid(data_number~.,scales = "free")+
   scale_x_continuous(name = "Time/1000")+scale_y_continuous(name = "RFU/1000")
-
-
